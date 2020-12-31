d6df8b23d6d7d5bb2531488ca6d71378eb3cfa6f
Artur <artur300998@gmail.com>
2020-12-31 17:59:15 +0100

    HTTP methods test. - controllers - routes - sample data in app's memory

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const IndexController = require('../controllers/IndexController');
+const ProductsController = require('../controllers/ProductsController')
 
-router.get('/', (req, res) => {
-    res.json({
-        'status': 'Working!!!'
-    });
-});
+router.get('/products', ProductsController.getAll);
+router.get('/products/:id', ProductsController.getById);
+router.post('/products', ProductsController.store);
+router.put('/products/:id', ProductsController.updateById);
 
 module.exports = router;
